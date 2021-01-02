f851389d529e6e8864f01bac035ec008aca6d997
HelenSobi <helensobia88@gmail.com>
2021-01-02 14:18:45 +0530

    machine-test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module'
 import { LayoutCardComponent } from './layout-card/layout-card.component';
+import { KeyFeaturesComponent } from './key-features/key-features.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutCardComponent
+    LayoutCardComponent,
+    KeyFeaturesComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
